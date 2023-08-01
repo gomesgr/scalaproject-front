09901144816dddb4acdbf7bca62c08484f3ac170
Gabriel <gomes.g.r@outlook.com>
2023-08-01 19:43:33 -0300

    POST colocado
    Agora quando um usuário loga, seus dados (nome, usuário e email) são salvos no banco de dados

--- src/components/BtnGoogleLogin.tsx
+++ src/components/BtnGoogleLogin.tsx
@@ -1,30 +1,61 @@
-import { BiLogoGoogle } from "react-icons/bi";
+import { BiLogoGoogle } from 'react-icons/bi'
 
-import { useGoogleLogin } from '@react-oauth/google';
-import axios from "axios";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-
+import { useGoogleLogin } from '@react-oauth/google'
+import axios from 'axios'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { GoogleProfile, urlMembro } from './Constants'
 
 export default function BtnGoogleLogin(props: any) {
     const navigate = useNavigate()
-    
+
+    // TODO
+    // Quando o usuário loga denovo, retorna o código 500 (pois já existe um usuário na tabela)
+    const saveProfile = () => {
+        try {
+            const profile: GoogleProfile = JSON.parse(
+                localStorage.getItem('perfil')!
+            )
+            console.log(profile)
+            axios
+                .post(`${urlMembro}/${profile.email}`, {
+                    nome: profile.given_name,
+                    usuario: profile.name,
+                    administrador: '0',
+                })
+                .then((resp) => console.log(resp))
+                .catch((err) => console.error(err))
+        } catch (ex) {
+            console.error(ex)
+        }
+    }
+
     useEffect(() => {
         if (localStorage.getItem('perfil')) {
             navigate('/calendario')
         }
         if (props.usuario) {
-            axios.get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${props.usuario.access_token}`, {
-                headers: {
-                    Authorization: `Bearer ${props.usuario.acces_token}`,
-                    Accept: 'application/json'
-                }
-            }).then((resp) => {
-                console.log(resp.data)
-                props.setPerfil(resp.data)
-                localStorage.setItem('perfil', JSON.stringify(resp.data))
-            })
-                .then((_) => localStorage.setItem('usuario', JSON.stringify(props.usuario)))
+            axios
+                .get(
+                    `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${props.usuario.access_token}`,
+                    {
+                        headers: {
+                            Authorization: `Bearer ${props.usuario.acces_token}`,
+                            Accept: 'application/json',
+                        },
+                    }
+                )
+                .then((resp) => {
+                    props.setPerfil(resp.data)
+                    localStorage.setItem('perfil', JSON.stringify(resp.data))
+                })
+                .then((_) =>
+                    localStorage.setItem(
+                        'usuario',
+                        JSON.stringify(props.usuario)
+                    )
+                )
+                .then((_) => saveProfile())
                 .then((_) => navigate('/calendario'))
                 .catch((err) => console.error(err))
         }
@@ -34,18 +65,22 @@ export default function BtnGoogleLogin(props: any) {
         onSuccess: (resp) => {
             props.setUsuario(resp)
         },
-        onError: (error) => console.error('Erro', error)
+        onError: (error) => console.error('Erro', error),
     })
 
-    return <button onClick={() => login()}>
-        <div id='loginBtn' className="flex flex-row place-content-center items-center">
-            <span className="relative flex h-3 w-3">
-                <span className="animate-ping absolute left-5/6 bottom-5 inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
-                <span className="relative inline-flex left-5/6 bottom-5 rounded-full h-3 w-3 bg-red-400"></span>
-            </span>
-            <BiLogoGoogle size={50} />
-            Entre com o Google
-        </div>
-
-    </button>
+    return (
+        <button onClick={() => login()}>
+            <div
+                id='loginBtn'
+                className='flex flex-row place-content-center items-center'
+            >
+                <span className='relative flex h-3 w-3'>
+                    <span className='animate-ping absolute left-5/6 bottom-5 inline-flex h-full w-full rounded-full bg-red-400 opacity-75'></span>
+                    <span className='relative inline-flex left-5/6 bottom-5 rounded-full h-3 w-3 bg-red-400'></span>
+                </span>
+                <BiLogoGoogle size={50} />
+                Entre com o Google
+            </div>
+        </button>
+    )
 }
--- src/components/Constants.ts
+++ src/components/Constants.ts
@@ -1,37 +1,35 @@
 export type Funcao = {
-    id: number,
-    nome: string,
+    id: number
+    nome: string
     exerceSet: Array<{
-        id: number,
-        membro: any,
+        id: number
+        membro: any
         funcao: any
     }>
 }
 export const urlFuncao: string = 'http://localhost:8080/api/funcao'
 
 export type Membro = {
-    id: number,
-    nome: string,
-    telefone: string,
-    ativo: string,
-    administrador: string,
-    usuario: string,
-    provedor: string,
+    id: number
+    nome: string
+    email: string
+    administrador: string
+    usuario: string
     exerceSet: Array<{
-        id: number,
-        membro: any,
+        id: number
+        membro: any
         funcao: any
     }>
 }
 export const urlMembro: string = 'http://localhost:8080/api/membro'
 
 export type CultosBool = {
-    culto: Culto | null,
+    culto: Culto | null
     existe: boolean
 }
 
 export type Culto = {
-    data: number,
+    data: number
     nome: string
 }
 export const urlCulto: string = 'http://localhost:8080/api/culto'
@@ -45,37 +43,40 @@ export interface MembroObject {
 }
 
 export type Exerce = {
-    id: number,
-    membro: Membro,
+    id: number
+    membro: Membro
     funcao: Funcao
 }
 
 export type Trabalha = {
-    id: number,
-    exerce: Exerce,
+    id: number
+    exerce: Exerce
     culto: Culto
 }
 
 export const urlTrabalha: string = 'http://localhost:8080/api/trabalha'
 
 export type GoogleUser = {
-    access_token: string,
-    authuser: string,
-    expires_in: number,
-    prompt: string,
-    scope: string,
+    access_token: string
+    authuser: string
+    expires_in: number
+    prompt: string
+    scope: string
     token_type: string
 }
 
 export type GoogleProfile = {
-    email: string,
-    family_name: string,
-    given_name: string,
-    id: string,
-    locale: string,
-    name: string,
-    picture: string,
+    email: string
+    family_name: string
+    given_name: string
+    id: string
+    locale: string
+    name: string
+    picture: string
     verified_email: boolean
 }
 
-export interface ChaveValorTrabalha {data: number, valor: Trabalha}
+export interface ChaveValorTrabalha {
+    data: number
+    valor: Trabalha
+}
